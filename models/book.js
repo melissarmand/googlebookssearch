344f1f2f7ffd84ddf61edbbd83373a43ba3ea436
melissarmand <82781751+melissarmand@users.noreply.github.com>
2021-10-06 05:45:52 -0400

    added models files

---
+++ models/book.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  id: {
+      type: String,
+      required: true,
+      unique: true
+  },
+
+  title: {
+      type: String,
+      required: true
+  },
+
+  authors: {
+      type: String,
+      required: true
+  },
+
+  description: {
+      type: String,
+      required: true
+  },
+
+  image: {
+      type: String,
+      default: ""
+  },
+
+  link: {
+      type: String,
+      default: ""
+  }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
